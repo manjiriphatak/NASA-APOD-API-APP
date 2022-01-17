af468678af395aa8b681cc949c35e3f9bf682b72
manjiriphatak <85757379+manjiriphatak@users.noreply.github.com>
2022-01-17 01:07:30 -0500

    displayed the picture and details from the api

--- src/DisplayImage.js
+++ src/DisplayImage.js
@@ -5,22 +5,28 @@ export default function DisplayImage() {
   const [data, setData] = useState({ ready: false });
 
   function getImage(response) {
+    console.log(response.data);
     setData({
       ready: true,
       description: response.data.explanation,
       title: response.data.title,
+      image: response.data.hdurl,
+      date: response.data.date,
     });
   }
 
   if (data.ready) {
     return (
       <div>
-        <p>{data.title}</p>
+        <h3>{data.date}</h3>
+        <img src={data.image} width="300" alt={data.title} />
+        <h2>{data.title}</h2>
+        <p>{data.description}</p>
       </div>
     );
   } else {
     const apiKey = "G99EwI382vfUipXDiZLbTkdYDsZW9hAKISRgbnbt";
-    const apiUrl = `https://api.nasa.gov/planetary/apod?api_key=${apiKey}&date=2022-01-15&concept_tags=True`;
+    const apiUrl = `https://api.nasa.gov/planetary/apod?api_key=${apiKey}`;
     axios.get(apiUrl).then(getImage);
     return "loaded";
   }
